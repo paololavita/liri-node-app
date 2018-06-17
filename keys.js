ae88229b8542a815fe4c5ee6760ab50d761958ad
Jonas Grumby <plavita@hotmail.com>
2018-06-17 10:12:44 -0400

    3rd Push

--- keys.js
+++ keys.js
@@ -1,10 +1,11 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.Mq1YI9Uy7LV9xmnM8KaEG7LUU,
-  consumer_secret: process.env.AjAffxkcVWnYxF1LgBFPzQuUcBghnZTtTWiUP7Ay740zCp9d4y,
-  access_token_key: process.env.1008341472008077312-8xqdDuVasWMZJYNw9TTh9ofBE3n1gS,
-  access_token_secret: process.env.yMCBR8nbNzMUxVnvZq26ZfEtaxCRc746aQHkZx4zkIyB1
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  
 };
 
 exports.spotify = {
