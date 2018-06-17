b742c044f1a0d6f4be11427cecbcf2838d37ffa3
Jonas Grumby <plavita@hotmail.com>
2018-06-17 10:06:43 -0400

    Second Push

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.Mq1YI9Uy7LV9xmnM8KaEG7LUU,
+  consumer_secret: process.env.AjAffxkcVWnYxF1LgBFPzQuUcBghnZTtTWiUP7Ay740zCp9d4y,
+  access_token_key: process.env.1008341472008077312-8xqdDuVasWMZJYNw9TTh9ofBE3n1gS,
+  access_token_secret: process.env.yMCBR8nbNzMUxVnvZq26ZfEtaxCRc746aQHkZx4zkIyB1
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
